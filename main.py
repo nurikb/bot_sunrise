091e1fee3c6d1b195daf5f7804e6e97c037a3ec5
nurikb <56222683+nurikb@users.noreply.github.com>
2021-08-29 18:26:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, request
 import os
 
 
-client = telebot.TeleBot(token)
+client = telebot.TeleBot(token=token)
 server = Flask(__name__)
 
 user_agency = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 OPR/78.0.4093.147'
@@ -36,13 +36,18 @@ def get_link(html, mname):
     return link_list
 
 
+@client.message_handler(commands=['start'])
+def send_welcome(message):
+    client.reply_to(message, 'добро пожаловать')
+
+
 @client.message_handler(content_types=['text'])
 def get_text(message):
     client_message = message.text
     link_list = get_link(get_html_data(client_message), client_message)
     if len(link_list) > 5:
         for link in link_list[:5]:
-            client.send_message(message.chat.id, link)
+            client.reply_to(message.chat.id, link)
     else:
         for link in link_list:
             client.send_message(message.chat.id, link)
@@ -57,14 +62,11 @@ def getMessage():
 @server.route("/")
 def webhook():
     client.remove_webhook()
-    client.set_webhook(url='https://boiling-sea-93295.herokuapp.com/' + token)
+    client.set_webhook(url='https://sunrise-bot.herokuapp.com')
     return "!", 200
 
 
 if __name__ == '__main__':
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
-
-
 
 
